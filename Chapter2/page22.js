b8d230836d0f81b020298fcd67f5a803e7720ffd
Andrii Yatsola <andry.yatsola@gmail.com>
2022-05-27 17:42:41 +0300

    capter2

---
+++ Chapter2/page22.js
@@ -0,0 +1,14 @@
+var myName = "Andrii";
+
+var codeWord1 = "обернись";
+var codeWord2 = "неужели";
+var codeWord3 = "огурцы";
+var codeWord4 = "липкие";
+var codeWord5 = "?!";
+codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1];
+
+var string = "эЙ, кАК деЛа?";
+var lowerCaseString= string.toLowerCase ();
+var firstLetter = lowerCaseString[0].toUpperCase ();
+lowerCaseString = lowerCaseString.slice(1);
+firstLetter + lowerCaseString;
---
+++ Chapter2/page23.js
@@ -0,0 +1,4 @@
+var age = 11;
+var accompanied = true;
+
+var ageLimit = 12;
